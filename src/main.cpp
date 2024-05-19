2e121bcc987cabe7229d3863059dd71d3d0ce319
Zachary Pike <zachturnpike@gmail.com>
2024-05-19 14:53:03 -0400

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,23 @@
 #include <X11/Xlib.h>
 #include <bits/stdc++.h>
+#include <ctime>
+#include "fmt/core.h"
+#include "fmt/chrono.h"
 
 int main() {
     Display* dpy = XOpenDisplay(NULL);
 
-    char * str = "Hello world";
-
     while(true) {
         std::ifstream battCapacityFile("/sys/class/power_supply/BAT0/capacity");
-
-        std::stringstream ss;
-
         int batCap;
         battCapacityFile >> batCap;
 
-        ss << batCap << '%';
+        auto time = std::chrono::system_clock::now();
+        std::time_t t = std::chrono::system_clock::to_time_t(time);
 
-        std::string final = ss.str();
-        XStoreName(dpy, DefaultRootWindow(dpy), final.c_str());
+
+        std::string output = fmt::format("Battery: {} | Date: {:%A, %B %d %Y | %r}", batCap, fmt::localtime(t));
+        XStoreName(dpy, DefaultRootWindow(dpy), output.c_str());
 	    XSync(dpy, False);
 
         std::this_thread::sleep_for(std::chrono::milliseconds(200));
